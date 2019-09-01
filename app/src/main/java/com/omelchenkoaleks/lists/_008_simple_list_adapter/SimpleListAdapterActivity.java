4adc0a872a5499a0f256c8f58593c6364ffe5705
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-09-01 10:00:18 +0300

    SimpleAdapter, но уже с использование объектов ...

--- app/src/main/java/com/omelchenkoaleks/lists/_008_simple_list_adapter/SimpleListAdapterActivity.java
+++ app/src/main/java/com/omelchenkoaleks/lists/_008_simple_list_adapter/SimpleListAdapterActivity.java
@@ -20,20 +20,34 @@ public class SimpleListAdapterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_008_simple_list_adapter);
 
-        String[] values = {"text1", "text2", "text3", "text4", "text5"};
-        boolean[] checked = {false, true, false, true, true};
+//        String[] values = {"text1", "text2", "text3", "text4", "text5"};
+//        boolean[] checked = {false, true, false, true, true};
         int img = R.mipmap.ic_launcher_round;
+
+        MyObj[] objs = {new MyObj("text1", false, img),
+                new MyObj("text2", false, img),
+                new MyObj("text3", true, img),
+                new MyObj("text4", false, img),
+                new MyObj("text5", true, img)};
 
         List<Map<String, Object>> valuesMapList = new ArrayList<>();
         Map<String, Object> currentMap;
 
-        for (int i = 0; i < 5; i++) {
+        for (MyObj obj : objs) {
             currentMap = new HashMap<>();
-            currentMap.put("text", values[i]);
-            currentMap.put("check", checked[i]);
-            currentMap.put("img", img);
+            currentMap.put("text", obj.getValue());
+            currentMap.put("check", obj.isCheck());
+            currentMap.put("img", obj.getImgId());
             valuesMapList.add(currentMap);
         }
+
+//        for (int i = 0; i < 5; i++) {
+//            currentMap = new HashMap<>();
+//            currentMap.put("text", values[i]);
+//            currentMap.put("check", checked[i]);
+//            currentMap.put("img", img);
+//            valuesMapList.add(currentMap);
+//        }
 
         String[] from = {"text", "check", "img"};
         int[] to = {R.id.simple_list_adapter_text_view,
@@ -44,5 +58,29 @@ public class SimpleListAdapterActivity extends AppCompatActivity {
 
         ListView listView = findViewById(R.id.simple_list_adapter_list_view);
         listView.setAdapter(simpleAdapter);
+    }
+
+    private class MyObj {
+        private String value;
+        private boolean check;
+        private int imgId;
+
+        public MyObj(String value, boolean check, int imgId) {
+            this.value = value;
+            this.check = check;
+            this.imgId = imgId;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public boolean isCheck() {
+            return check;
+        }
+
+        public int getImgId() {
+            return imgId;
+        }
     }
 }
