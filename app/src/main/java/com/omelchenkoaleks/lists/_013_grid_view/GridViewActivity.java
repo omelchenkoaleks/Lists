81d551b239196ccb3b7443f8c972f2610ffe5ea5
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-09-02 16:03:50 +0300

    эксперименты со слушателем ...

--- app/src/main/java/com/omelchenkoaleks/lists/_013_grid_view/GridViewActivity.java
+++ app/src/main/java/com/omelchenkoaleks/lists/_013_grid_view/GridViewActivity.java
@@ -1,14 +1,21 @@
 package com.omelchenkoaleks.lists._013_grid_view;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.omelchenkoaleks.lists.R;
+import com.omelchenkoaleks.lists._012_spinner.SpinnerActivity;
 
 public class GridViewActivity extends AppCompatActivity {
+    private static final String TAG = "GridViewActivity";
+
     private String[] data = {"grid", "view", "doctor", "sector", "price", "thing", "men", "women",
             "lite", "live", "love", "Dan", "don", "top"};
 
@@ -24,6 +31,17 @@ public class GridViewActivity extends AppCompatActivity {
                 R.layout.item_013_grid_view, R.id.grid_view_text_view, data);
         mGridView = findViewById(R.id.grid_view);
         mGridView.setAdapter(mAdapter);
+
+        mGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(GridViewActivity.this, "Position = " + position, Toast.LENGTH_SHORT).show();
+
+                if (position > 3) {
+                    Log.d(TAG, "Position > 3 = " + position);
+                }
+            }
+        });
 
         adjustGridView(); // без настроек будет просто обычный лист
     }
