a3cbd1a73c6d37f1f824d227944fd6e3dbb853e7
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-09-10 09:12:07 +0300

    ListFragment кастомный layout, onListItemClick, xml-параметры android:list and android:empty ...

--- app/src/main/java/com/omelchenkoaleks/lists/_015_list_fragment/MainFragment.java
+++ app/src/main/java/com/omelchenkoaleks/lists/_015_list_fragment/MainFragment.java
@@ -1,10 +1,17 @@
 package com.omelchenkoaleks.lists._015_list_fragment;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.ListFragment;
+
+import com.omelchenkoaleks.lists.R;
 
 public class MainFragment extends ListFragment {
     private String[] data = new String[] {"one", "two", "tree", "four"};
@@ -16,5 +23,16 @@ public class MainFragment extends ListFragment {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(),
                 android.R.layout.simple_list_item_1, data);
         setListAdapter(adapter);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_015_list_fragment, null);
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        Toast.makeText(getActivity(), "position = " + position, Toast.LENGTH_SHORT).show();
     }
 }
